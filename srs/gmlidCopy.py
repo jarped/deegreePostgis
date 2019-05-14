4b2ac9ceac9d71d8b35bbee95c7a5134acd58114
jarle pedersen <jarle.pedersen@norconsult.com>
2019-05-14 15:47:43 +0200

    added alterations to use localid remove uuidgenerator

--- srs/gmlidCopy.py
+++ srs/gmlidCopy.py
@@ -3,16 +3,22 @@ import sys
 from lxml import etree
 import os
 
-directory = os.getcwd()
+directory = sys.argv[1] #os.getcwd()
 ns={'ns0':'http://www.deegree.org/datasource/feature/sql'}
+localid='app_identifikasjon_app_identifikasjon_app_lokalid'
 
 for filename in os.listdir(directory):
   if not filename.endswith(".xml"): continue
-  tree = etree.parse(open(filename))
+  tree = etree.parse(open(directory + filename))
 
   for featureType in tree.xpath('//ns0:FeatureTypeMapping', namespaces=ns):
     name=featureType.get('name').replace(':','.') + '_'
     for fidMapping in featureType.xpath('./ns0:FIDMapping', namespaces=ns):
       fidMapping.set('prefix',name)
+      column = fidMapping.xpath('./ns0:Column', namespaces=ns)
+      column[0].set('name',localid)
+      uuidGenerator=fidMapping.xpath('./ns0:UUIDGenerator', namespaces=ns)
+      if(len(uuidGenerator) == 0): continue
+      fidMapping.remove(uuidGenerator[0])
 
-  tree.write(filename + '2', encoding='utf-8', pretty_print=True)
+  tree.write(directory + filename, encoding='utf-8', pretty_print=True)
