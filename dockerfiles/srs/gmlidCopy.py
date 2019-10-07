65863d8e1dc0fda054ba87b2f108c9461b57a0db
Jarle Pedersen <Jarle.Pedersen@geodata.no>
2019-10-07 14:39:48 +0200

    added reguleringsplan_2019040 for utm 32

--- dockerfiles/srs/gmlidCopy.py
+++ dockerfiles/srs/gmlidCopy.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 from lxml import etree
 import os
 
-directory = sys.argv[1] #os.getcwd()
+directory = os.getcwd() #sys.argv[1] 
 ns={'ns0':'http://www.deegree.org/datasource/feature/sql'}
 localid='app_identifikasjon_app_identifikasjon_app_lokalid'
 
 for filename in os.listdir(directory):
+  filePath = directory + "\\" + filename
   if not filename.endswith(".xml"): continue
-  tree = etree.parse(open(directory + filename))
+  tree = etree.parse(open(filePath, encoding="utf-8"))
 
   for featureType in tree.xpath('//ns0:FeatureTypeMapping', namespaces=ns):
     name=featureType.get('name').replace(':','.') + '_'
+    print(name)
     for fidMapping in featureType.xpath('./ns0:FIDMapping', namespaces=ns):
       fidMapping.set('prefix',name)
       column = fidMapping.xpath('./ns0:Column', namespaces=ns)
@@ -25,4 +28,4 @@ for filename in os.listdir(directory):
     for join in joins:
       join.set('fromColumns', localid)
 
-  tree.write(directory + filename, encoding='utf-8', pretty_print=True)
+  tree.write(filePath, encoding='utf-8', pretty_print=True)
